060e5c7d309ae28d3f297fde4e10661167e180e4
Hitesh Patil <hiteshpatil0205@gmail.com>
2025-06-26 10:53:01 +0530

    spotify clone

---
+++ Spotify Clone/script.js
@@ -0,0 +1,14 @@
+
+async function main(){
+    let a = await fetch('http://127.0.0.1:3000/songs/')
+    let response = await a.text();
+    console.log(response)
+    let div = document.createElement("div")
+    div.innerELement = response;
+    let as = div.getElementsByTagName("a")
+
+    
+    console.log(as)
+}
+
+main()
