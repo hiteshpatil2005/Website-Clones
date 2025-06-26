b98bfc406ee740616ccdc766e0fe8c46177c009d
Hitesh Patil <hiteshpatil0205@gmail.com>
2025-06-26 22:54:32 +0530

    spotify clone

--- Spotify Clone/script.js
+++ Spotify Clone/script.js
@@ -1,4 +1,22 @@
 
+let currentSong = new Audio();
+
+function convertToMinutesSeconds(totalSeconds) {
+
+    if (isNaN(totalSeconds) || totalSeconds < 0) {
+        return "Invalid input";
+    }
+
+    const minutes = Math.floor(totalSeconds / 60);
+    const seconds = Math.floor(totalSeconds % 60);
+
+    // Add leading zeros if needed
+    const formattedMinutes = String(minutes).padStart(2, '0');
+    const formattedSeconds = String(seconds).padStart(2, '0');
+
+    return `${formattedMinutes}:${formattedSeconds}`;
+}
+
 async function getSongs() {
     let a = await fetch('http://127.0.0.1:3000/songs/')
     let response = await a.text();
@@ -18,8 +36,23 @@ async function getSongs() {
     return songs;
 }
 
+const playMusic = (track, pause = false) => {
+    currentSong.src = "/songs/" + track;
+
+    if (!pause) {
+        currentSong.play();
+        play.src = "Assets/pause.svg";
+    }
+
+    document.querySelector(".songinfo").innerHTML = decodeURI(track)
+    document.querySelector(".songtime").innerHTML = "00:00 / 00:00";
+}
+
 async function main() {
+
+    //getting list of all the songs
     let songs = await getSongs()
+    playMusic(songs[0], true)
 
     //Show all the songs in the playist
     let songUL = document.querySelector(".songlist").getElementsByTagName("ul")[0]
@@ -34,6 +67,38 @@ async function main() {
                                     <img class="invert" src="Assets/play.svg" alt="play">
                                 </div></li>`;
     }
+
+    Array.from(document.querySelector(".songlist").getElementsByTagName("li")).forEach(e => {
+        e.addEventListener("click", element => {
+            console.log(e.querySelector(".info").firstElementChild.innerHTML)
+            playMusic(e.querySelector(".info").firstElementChild.innerHTML.trim())
+        })
+    })
+
+    //Attaching event listener to play,next and previous
+    play.addEventListener("click", () => {
+        if (currentSong.paused) {
+            currentSong.play();
+            play.src = "Assets/pause.svg";
+        }
+        else {
+            currentSong.pause();
+            play.src = "Assets/play.svg";
+        }
+    })
+
+    // Listen for time update event
+    currentSong.addEventListener("timeupdate", () => {
+        console.log(currentSong.currentTime, currentSong.duration);
+        document.querySelector(".songtime").innerHTML = `${convertToMinutesSeconds(currentSong.currentTime)} / ${convertToMinutesSeconds(currentSong.duration)}`;
+        document.querySelector(".circle").style.left = (currentSong.currentTime/ currentSong.duration) * 100 + "%";
+    })
+
+    document.querySelector(".seekbar").addEventListener("click", (e) => {
+        let percent = (e.offsetX/e.target.getBoundingClientRect().width) * 100 ;
+        document.querySelector(".circle").style.left = percent + "%";
+        currentSong.currentTime = (currentSong.duration) * percent / 100;
+    })
 }
 
 main()
